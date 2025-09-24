3ef993ae49294b28af3540bf534acde9934bcc58
SUPRADEEP VELADANDI <2300039091@kluniversity.in>
2025-09-24 10:56:35 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-16-171-40-199.eu-north-1.compute.amazonaws.com:8081/auth";
+const API_URL = "http://ec2-13-61-188-158.eu-north-1.compute.amazonaws.com:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
