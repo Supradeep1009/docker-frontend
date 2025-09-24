65244df1ec6bc4ec76bcae94e2d9ffaf6f3f463b
SUPRADEEP VELADANDI <2300039091@kluniversity.in>
2025-09-24 10:55:46 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-16-171-40-199.eu-north-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-13-61-188-158.eu-north-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://ec2-13-60-51-86.eu-north-1.compute.amazonaws.com/api/products/images/${imagePath}`;
+  return `http://ec2-13-61-188-158.eu-north-1.compute.amazonaws.com/api/products/images/${imagePath}`;
 };
